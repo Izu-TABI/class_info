f4b00ae5341bdd83fd7db25c389fd68cf665be6d
Izu-TABI <masakisama14@gmail.com>
2023-05-04 23:02:55 +0900

    Update: main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import requests
 import time
 import os
+import datetime
+import re
+import schedule
 from bs4 import BeautifulSoup
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
 
-def main():
+def schedle_test():
+  dt = datetime.datetime.now()
   CHANNEL_ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
   line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-  years = '2023'
-  month = '06'
+  # 年月日を取得する
+  years = str(dt.year)
+  if dt.month < 10:
+    month = "0" + str(dt.month)
+  else:
+    month = str(dt.month)
 
+  today = int(dt.day)
+
+  time.sleep(1)
   res = requests.get(
       f"https://www.tsuyama-ct.ac.jp/oshiraseVer4/renraku/renraku{years}{month}.html"
   )
@@ -27,13 +38,41 @@ def main():
 
   # 授業変更
   print(class_changes)
-
   # 休講情報
   print(canceled_class)
+
+  # 授業変更情報の整理
+  class_changes_split = class_changes.splitlines()
+  class_changes_send_arr = ""
+  # データを分割し、日付を取り出す。今日より後のものを配列に追加する。
+  for data in range(2, len(class_changes_split)):
+    split_month = re.split("\d月", class_changes_split[data])
+    split_date = split_month[1].split("日")
+    for date in range(0, len(split_date), 2):
+      date_int = int(split_date[date])
+      if date_int >= today:
+        class_changes_send_arr += "\n" + class_changes_split[data]+"\n"
+  print(class_changes_send_arr)
+
+  # 休講情報の整理
+  canceled_class_split = canceled_class.splitlines()
+  canceled_class_send_arr = ""
+  # データを分割し、日付を取り出す。今日より後のものを配列に追加する。
+  for data in range(2, len(canceled_class_split)):
+    split_month = re.split("\d月", canceled_class_split[data])
+    split_date = split_month[1].split("日")
+    for date in range(0, len(split_date), 2):
+      date_int = int(split_date[date])
+      if date_int >= today:
+        canceled_class_send_arr += "\n" + canceled_class_split[data]+"\n"
+  print(canceled_class_send_arr)
+
+  is_change = False
 
   f_changes = open('class_changes.txt', 'r', encoding='UTF-8')
   changes_old_data = f_changes.read()
   if class_changes != changes_old_data:
+    is_change = True
     print("授業変更の情報が更新されました。")
     f = open('class_changes.txt', 'w')
     f.write(class_changes)
@@ -42,14 +81,22 @@ def main():
   f_canceled = open('canceled_class.txt', 'r', encoding='UTF-8')
   canceled_old_data = f_canceled.read()
   if canceled_class != canceled_old_data:
+    is_change = True
     print("休講の情報が更新されました。")
     f = open('canceled_class.txt', 'w')
     f.write(canceled_class)
     f.close()
-  class_changes = class_changes[1:]
 
-  massages = TextSendMessage(text=class_changes + canceled_class)
-  line_bot_api.broadcast(messages=massages)
+  if is_change:
+    class_changes = class_changes[1:]
+    massages = TextSendMessage(
+        text="授業情報が更新されましたので、お知らせします。\n\n" + "▪︎授業変更" + class_changes_send_arr +
+        "\n\n▪︎休講" + canceled_class_send_arr)
+    line_bot_api.broadcast(messages=massages)
 
-if __name__ == "__main__":
-    main()
+
+schedule.every(7200).seconds.do(schedle_test)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
